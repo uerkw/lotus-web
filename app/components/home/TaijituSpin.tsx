d604468a86a38a7389603e3822f2b2623023dc80
uerkw <38047633+uerkw@users.noreply.github.com>
2024-08-12 18:04:52 -0400

    Hmmm, actually, Tailwind animations are kinda cringe

--- app/components/home/TaijituSpin.tsx
+++ app/components/home/TaijituSpin.tsx
@@ -1,5 +1,5 @@
 import taijitu from "/public/taijitu.svg";
-
+import styles from "./TaijituSpin.module.css";
 export function TaijituSpin() {
   return (
     <div className="flex justify-center items-center mt-4 mb-4">
@@ -7,7 +7,7 @@ export function TaijituSpin() {
         src={taijitu}
         // style={{ height: "96px" }}
         alt="Rotating Taijitu"
-        className="animate-spin origin-center h-32 md:h-48"
+        className={`${styles["animate-spin"]} origin-center h-32 md:h-48`}
       />
     </div>
   );
