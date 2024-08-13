018316c139d0d97c533d442162cd5ddd0f3baf57
uerkw <38047633+uerkw@users.noreply.github.com>
2024-08-13 13:22:59 -0400

    Fix rounded borders on buttons

--- app/components/nav/MobileDropdownMenu.tsx
+++ app/components/nav/MobileDropdownMenu.tsx
@@ -13,13 +13,22 @@ export default function MobileDropdownMenu(props: MenuProps) {
           >
             Home
           </a>
-          <a href="/about" className="px-4 py-2 text-white hover:bg-blue-500">
+          <a
+            href="/about"
+            className="px-4 py-2 text-white rounded-md hover:bg-blue-500"
+          >
             About
           </a>
-          <a href="/blog" className="px-4 py-2 text-white hover:bg-blue-500">
+          <a
+            href="/blog"
+            className="px-4 py-2 text-white rounded-md hover:bg-blue-500"
+          >
             Blog
           </a>
-          <a href="/wiki" className="px-4 py-2 text-white hover:bg-blue-500">
+          <a
+            href="/wiki"
+            className="px-4 py-2 text-white rounded-md hover:bg-blue-500"
+          >
             Wiki
           </a>
         </div>
