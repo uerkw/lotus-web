173c534306a51091d5768b2b2a3b4e3d07f3e515
uerkw <38047633+uerkw@users.noreply.github.com>
2024-08-13 11:45:25 -0400

    Remove tailwind config for spin animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,14 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      animation: {
-        spin: "spin 5s linear infinite",
-      },
-      keyframes: {
-        spin: {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(360deg)" },
-        },
+      // animation: {
+      //   spin: "spin 5s linear infinite",
+      // },
+      // keyframes: {
+      //   spin: {
+      //     "0%": { transform: "rotate(0deg)" },
+      //     "100%": { transform: "rotate(360deg)" },
+      //   },
       },
     },
   },
